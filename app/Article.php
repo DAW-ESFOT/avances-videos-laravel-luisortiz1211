7335c84fd4dcdfd32ea5765590942ee6e4496b34
luisortiz1211 <66312625+luisortiz1211@users.noreply.github.com>
2020-12-01 00:30:01 -0500

    #4 #5 Seeding tables

--- app/Article.php
+++ app/Article.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'body'];
-    //fillable campos que puedes editados
+    //fillable campos que puedes editador a traves de la base de datos
 }
