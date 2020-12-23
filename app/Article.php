1eaf605c98c397e693512567d4f4a5ea1cc6ec94
luisortiz1211 <66312625+luisortiz1211@users.noreply.github.com>
2020-12-23 10:48:17 -0500

    #12 Relaciones eloquent

--- app/Article.php
+++ app/Article.php
@@ -8,4 +8,11 @@ class Article extends Model
 {
     protected $fillable = ['title', 'body'];
     //fillable campos que puedes editador a traves de la base de datos
+
+    public function comments (){
+        return $this->hasmany('App\Comment');
+    }
+    public function user (){
+        return $this->belongsto('App\User');
+    }
 }
--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['text'];
+
+    public function user (){
+        return $this->belongsto('App\User');
+    }
+    public function article (){
+        return $this->belongsto('App\Article');
+    }
 }
